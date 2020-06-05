99be1626fbafb3300b848395728d72fe5987cd75
ipractice108 <dkorolev.pm@gmail.com>
2020-06-05 14:32:56 +0530

    last changes

--- MON_SCHEDULE.py
+++ MON_SCHEDULE.py
@@ -5,10 +5,12 @@ import schedule
 import time
 
 def run_schedule():
+    print("running_schedule")
     monday.mon_schedule()
     thursday.thu_schedule()
     friday.fri_schedule()
 
     while True:
+        print("schedule_going")
         schedule.run_pending()
         time.sleep(10)
