35ec9daa0e4ef8b83a6e923ceaa48230fea6f49a
Key <mr.kirill.kruglov@gmail.com>
2020-06-05 10:28:09 +0300

    fix small schedule bugs

--- MON_SCHEDULE.py
+++ MON_SCHEDULE.py
@@ -1,15 +1,14 @@
 import monday
 import thursday
+import friday
 import schedule
 import time
 
 
-def run_mon_schedule():
+def run_schedule():
     monday.mon_schedule()
-
-def run_thu_schedule():
     thursday.thu_schedule()
-
+    friday.fri_schedule()
 
     while True:
         schedule.run_pending()
