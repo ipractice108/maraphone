85218a83da23c1eb61138740b45fe41564c6b8ca
ipractice108 <dkorolev.pm@gmail.com>
2020-06-07 22:07:42 +0530

    last changes

--- MON_SCHEDULE.py
+++ MON_SCHEDULE.py
@@ -1,14 +1,23 @@
 import monday
+import tuesday
+import wednsday
 import thursday
 import friday
+import saturday
+import sunday
 import schedule
 import time
 
 def run_schedule():
     print("running_schedule")
     monday.mon_schedule()
+    tuesday.tue_schedule()
+    wednsday.wed_schedule()
     thursday.thu_schedule()
     friday.fri_schedule()
+    saturday.sat_schedule()
+    sunday.sun_schedule()
+
 
     while True:
         print("schedule_going")
