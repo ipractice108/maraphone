a7504238c2c3c62fbca234ebfabf69e41ebf6b4c
ipractice108 <dkorolev.pm@gmail.com>
2020-09-14 11:13:57 +0300

    last changes

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-token = "1123534002:AAFt9fbrqlxTEYeiiusO6mtUqma-H2Z8psQ"
+token = "1123534002:AAEgn2VIN9O1OzyPebu6FtERsHZkKJsXO-8"
 heroku_url = "https://maraphone.herokuapp.com/"
 bot_database = "bot_db.sqlite3"
 
